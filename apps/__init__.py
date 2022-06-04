3e28f730967f9d98b9ca436071ef333801750db3
Тимошенков Сергей Валентинович <s.timoshenkov@tinkoff.ru>
2022-06-04 21:40:18 +0300

    add heroku fix

--- apps/__init__.py
+++ apps/__init__.py
@@ -36,7 +36,7 @@ def register_blueprints(app: Flask) -> None:
         app.register_blueprint(module.blueprint)
 
 
-def create_app(config) -> Flask:
+def create_app(Config) -> Flask:
     """
     Создает и возвращает Flask приложение
     """
