1755675b39581d5e79d7cd2b141087aed76320d8
Тимошенков Сергей Валентинович <s.timoshenkov@tinkoff.ru>
2022-06-04 21:55:04 +0300

    add heroku fix

--- Config.py
+++ Config.py
@@ -12,6 +12,10 @@ class Config(object):
     TESTING = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ENV = 'development'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if not os.environ.get('DATABASE_URL_LOCAL'):
+         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')\
+             .replace('postgres://', 'postgresql://')
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_LOCAL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
